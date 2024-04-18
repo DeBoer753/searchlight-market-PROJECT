c8bfdc5e83cc9e995d811147d30e3685d247d1e8
Myles DeBoer <mylesdeboer@Myless-MacBook-Pro-2.local>
2024-04-18 08:09:24 -0700

    edited backside card data to link more with cards. now easier to edit text from data file

--- src/Cards/A-BacksideData.js
+++ src/Cards/A-BacksideData.js
@@ -1,5 +1,5 @@
-// BACKSIDE CARD DATA
-export const description1 = "Hello, and welcome to Searchlight Market! My name is Myles and I am a full time sales rep / web developer hobbyist who created the card you are seeing now."
-export const description2 = "My goal is to help grow not only our portfolio of wines, beers, and spirits, but more importantly to grow the independent retail shops we know and love here in San Francisco through a digital perspective."
+// BACKSIDE DATA
+export const description1 = "Hello, and welcome to Searchlight Market! My name is Myles and I am a full time beverage sales rep / web developer hobbyist who created the card you are seeing now."
+export const description2 = "My goal is to help grow not only our portfolio of wines, beers, and spirits, but more importantly to grow the neighborhood stores we know and love here in San Francisco through a digital perspective."
 export const message = '"All of us at Searchlight Market are committed to provide the very best service and advice on wines, beers, spirts, and any other store items for your needs. If you have any questions, we are here to help!"'
 export const rescanTheQRCodeFor = "Rescan the QR codes during holidays and public events for updated themes and messages!"
