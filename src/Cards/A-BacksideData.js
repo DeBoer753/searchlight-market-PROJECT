b248f962d6c1cf0619513643c30055ce40f5d73e
Myles DeBoer <mylesdeboer@Myless-MacBook-Pro-2.local>
2024-09-23 20:42:43 -0700

    fall season update

--- src/Cards/A-BacksideData.js
+++ src/Cards/A-BacksideData.js
@@ -3,5 +3,5 @@ export const storeName = "SEARCHLIGHT MARKET"
 export const description1 = "Hello, and welcome to Searchlight Market! My name is Myles and I am a full time beverage sales rep / web developer hobbyist who created the card you are seeing now."
 export const description2 = "My goal is to help grow not only our portfolio of wines, beers, and spirits, but more importantly to grow the neighborhood stores we know and love here in San Francisco through a digital perspective."
 // export const message = '"All of us at Searchlight Market are committed to provide the very best service and advice on wines, beers, spirts, and any other store items for your needs. If you have any questions, we are here to help!"'
-export const message = '"New cards update now showing photographs best suited for your mood. Take a lap around the wine and beer shelves to check them out!"'
+export const message = '"It is that time of year again 🍁 Cheers to another cozy Fall season! A big thank you as well to everyone scanning the cards. We are so glad you are all enjoying them :)"'
 export const rescanTheQRCodeFor = "Rescan the QR codes during holidays and public events for updated themes and messages!"
