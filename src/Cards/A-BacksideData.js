d8fff61b169b638c9fa503dd9f72d4241019cd6c
deboer753 <myles.deboer@gmail.com>
2025-04-08 20:32:52 -0700

    updated message

--- src/Cards/A-BacksideData.js
+++ src/Cards/A-BacksideData.js
@@ -3,5 +3,5 @@ export const storeName = "SEARCHLIGHT MARKET"
 export const description1 = "Hello, and welcome to Searchlight Market! My name is Myles and I am a full time beverage sales rep / web developer hobbyist who created the card you are seeing now."
 export const description2 = "My goal is to help grow not only our portfolio of wines, beers, and spirits, but more importantly to grow the neighborhood stores we know and love here in San Francisco through a digital perspective."
 // export const message = '"All of us at Searchlight Market are committed to provide the very best service and advice on wines, beers, spirts, and any other store items for your needs. If you have any questions, we are here to help!"'
-export const message = '"Happy Spring Searchlight Market friends and family, and cheers..to summer - which is right around the corner!☀️"'
+export const message = '"Happy Spring, Searchlight Market friends and family, and cheers..to summer - which is right around the corner!☀️"'
 export const rescanTheQRCodeFor = "Rescan the QR codes during holidays and public events for updated themes and messages!"
